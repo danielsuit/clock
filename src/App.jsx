75adf60d9c0ca3649614292e4df957edad0c88f6
Daniel Suit <106452651+danielsuit@users.noreply.github.com>
2025-05-12 17:09:21 -0400

    modified:   .firebase/hosting.ZGlzdA.cache 	modified:   src/App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -13,19 +13,23 @@ function App() {
     setCurrentTime(time)
   }
   setInterval(updateTime, 1000);
-  const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
-  var d = new Date(),
-  hours = d.getHours(),
-  minutes = d.getMinutes();
-  if(minutes < 10){
-    minutes = "0"+minutes;
-  }
-  if(hours < 10){
-    hours = "0"+hours;
-  }
-  return (
-    <div className="App">
-        <div className="Clock"><span> {hours}</span><p className="colon">:</p><span>{minutes}</span></div>
+    const months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+    var d = new Date(),
+    hours = d.getHours(),
+    minutes = d.getMinutes();
+    if(minutes < 10){
+      minutes = "0"+minutes;
+    }
+    if(hours < 10){
+      hours = "0"+hours;
+    }
+    return (
+      <div className="App">
+        <div className="Time">
+            <span>boston</span>
+            {/* <span className="material-symbols-outlined">cloud</span> */}
+            <span> {hours}<span className="colon">:</span>{minutes}</span>
+        </div>
     </div>
   )
 }
